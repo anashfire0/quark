65cf46e746ce83605c4add5f5e98a16069889d9c
Ashfaq Rahman <ashrmzn22@gmail.com>
2018-05-18 18:45:13 +0530

    precautionary commit before profile model migration

--- users/models.py
+++ users/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
-
+from django.core.validators import RegexValidator
 # Create your models here.
+
 
 class CustomUserManager(UserManager):
     pass
+
 
 class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
         return f'{self.username} - {self.get_full_name()}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    profile_pic = models.ImageField('Profile picture',
+        upload_to='static-storage/user/profile_pic', blank=True)
+    phone_no = models.CharField('Phone No.', max_length=17, blank=True)
+    dob = models.DateField('Date Of Birth', blank=True, null=True)
